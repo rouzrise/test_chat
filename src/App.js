83b12fd02a5abb9d3995b49af260bf898196bb25
Rouzrise <rouzrise@gmail.com>
2018-09-27 02:04:04 +0300

    Add test chatkit functionality in action

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import MessageList from './Components/MessageList';
 import Chatkit from '@pusher/chatkit';
 import './App.css';
+import {instanceLocator, tokenUrl} from './config';
 
 
 class App extends Component {
 componentDidMount() {
-  const chatManager = new Chatkit.chatManager ({
+  const chatManager = new Chatkit.ChatManager ({
+    instanceLocator: instanceLocator,
+    userId: 'Ann',
+    tokenProvider: new Chatkit.TokenProvider({
+      url: tokenUrl
+    })
+    
+  })
 
+  chatManager.connect()
+  .then(currentUser => {
+    currentUser.subscribeToRoom ({
+      roomId: 17176885,
+      messageLimit: 20,
+      hooks: {
+        onNewMessage: message => {
+          console.log('message text: ', message.text);
+        }
+      }
+    })
   })
 }
   render() {
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const instanceLocator ='v1:us1:7bde51ee-52e3-4ccb-89c2-9e0f02f397a9';
 const secretKey = '148f8759-204a-4184-9350-0887e0e90428:kRFzlVPsITVQEt3JoB3Hi1vmljStABHBdURFqTn0hCo=';
-const tokenUrl;
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/7bde51ee-52e3-4ccb-89c2-9e0f02f397a9/token';
 
 export {instanceLocator, tokenUrl}
