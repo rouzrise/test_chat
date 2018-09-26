e670d177536a2ed189e6f0d4cfd8230862fe569f
Rouzrise <rouzrise@gmail.com>
2018-09-27 01:38:39 +0300

    Add @pusher/chatkit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import MessageList from './Components/MessageList';
+import Chatkit from '@pusher/chatkit';
 import './App.css';
 
 
 class App extends Component {
+componentDidMount() {
+  const chatManager = new Chatkit.chatManager ({
 
+  })
+}
   render() {
     return (
      <MessageList/>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const instanceLocator ='v1:us1:7bde51ee-52e3-4ccb-89c2-9e0f02f397a9';
+const secretKey = '148f8759-204a-4184-9350-0887e0e90428:kRFzlVPsITVQEt3JoB3Hi1vmljStABHBdURFqTn0hCo=';
+
